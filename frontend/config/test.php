28e85999e53b3897bfc9d9682a85b8db455e18d0
Abeer Saeed <abeersaeed21.91@gmail.com>
2017-02-09 23:18:19 +0500

    unit testing added

--- frontend/config/test.php
+++ frontend/config/test.php
@@ -1,12 +1,17 @@
 <?php
 return [
     'id' => 'app-frontend-tests',
+    'basePath' => dirname(__DIR__),
     'components' => [
         'assetManager' => [
             'basePath' =>__DIR__ . '/../assets',
         ],
         'db' => [
+        	'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;port=3305;dbname=demo',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
         ]
     ],
 ];
